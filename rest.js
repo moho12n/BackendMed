77f8092606b56fcfd7b50ff925b7da6bdd6487e0
AbdenourDAHMANI <ga_dahmani@esi.dz>
2020-02-18 18:54:18 +0100

    second edit

--- rest.js
+++ rest.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'tp4'
+    database: 'suivis_intervention_maintenance'
 });
 connection.connect();
 
@@ -18,44 +18,11 @@ connection.connect();
 
 
 // rest service
-app.get('/getplayers', function (req, res) {
-    var query = "select * from player";
-    connection.query(query, function (error, results) {
-        if (error) throw error;
-        res.send(results);
-    });
+app.get('/', function (req, res) {
+    
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+    
 });
-
-
-app.get('/getplayers/:id_player', function (req, res) {
-    var query = "select * from player where id_team = " + req.params.id_player;
-    connection.query(query, function (error, results) {
-        if (error) throw error;
-        res.send(results);
-    });
-});
-
-app.get('/getteams', function (req, res) {
-    var query = "select * from team";
-    connection.query(query, function (error, results) {
-        if (error) throw error;
-        res.send(results);
-    })
-});
-
-app.post('/addteam', function (req, res) {
-    console.log(req.body);
-    var team = JSON.parse(JSON.stringify(req.body));
-    var query = "INSERT INTO team VALUES (?,?,?)";
-    connection.query(query, [team.id_team, team.team_name, team.continent], function (error, results) {
-        if (error) {
-            res.send("ERROR")
-        }
-        else {
-            res.send("SUCCES")
-        }
-    });
-})
 // server creation
 
 var server = app.listen(8082, function () {
