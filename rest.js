69c210930524121e141ce73bb3b6c9f4a9cbd4e2
Moho <gm_douzane@esi.dz>
2020-03-07 03:35:52 +0100

    final one

--- rest.js
+++ rest.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'promeddb'
+    database: 'projetmobile'
 });
 connection.connect();
 
